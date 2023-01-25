c1f9924157f90911072775c84587b4d87e0450f3
TommyOliveir <laridatommy@gmail.com>
2023-01-25 15:13:11 +0100

    add

--- no_duplicates.js
+++ no_duplicates.js
@@ -28,7 +28,7 @@ function removeDupesFromArray(arr){
     // create a new arr to hold unique items 
     // for each item in recipe arr
         // if the item is NOT yet in the unique arr, push it in
-        // if it is in the unique arr, move on to the next item (do nothing)
+        // if it is in the unique arr, move on to the next item (do nothing),
     // return the unique arr 
     
     const uniqueItems = []; 
