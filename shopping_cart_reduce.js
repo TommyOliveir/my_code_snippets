18fba3a2de2a3d3a7277af72aa3b5f4bcc520265
TommyOliveir <laridatommy@gmail.com>
2023-01-31 15:44:27 +0100

    ok for today

---
+++ shopping_cart_reduce.js
@@ -0,0 +1,55 @@
+const shoppingCart = [
+    {
+        item: "🍭",
+        price: 2.99,
+        type: "sweet",
+        onSale: true
+    },
+    {
+        item: "🍫",
+        price: 1.99, 
+        type: "sweet",
+        onSale: true
+    },
+    {
+        item: "🥫",
+        price: 1.99, 
+        type: "savory",
+        onSale: false
+    },
+    {
+        item: "🍬",
+        price: .89, 
+        type: "sweet",
+        onSale: false
+    },
+    {
+        item: "🥦",
+        price: 3.99,
+        type: "savory",
+        onSale: false
+    }, 
+    {
+        item: "🍖",
+        price: 3.99, 
+        type: "savory",
+        onSale: true
+    }, 
+]
+
+/*  
+Use reduce() to total the groceries. 
+Then find a method that will round the total to 2 decimal places.
+
+Example output: 73.44
+*/
+
+function total(arr){
+    const total = arr.reduce((acc, curr) => {
+        return acc + curr.price;
+    }, 0);
+    
+   return total.toFixed(2);
+}
+
+console.log(total(shoppingCart));
