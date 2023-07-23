62c85623a73672b7b691d74651f902324c73d64c
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-07-23 12:55:40 +0200

    Create for-in-loop.js

---
+++ for-in-loop.js
@@ -0,0 +1,16 @@
+const users = 
+  {
+    id: 1,
+    FIRST_NAME: "Giraldo",
+    LAST_NAME: "Garmey",
+    EMAIL: "ggarmey0@washington.edu",
+    GENDER: "Male",
+    birthdate: "07-Jul-1962",
+  }
+;
+
+
+for(let item in users) {
+ console.log(item)
+ console.log(users[item])
+};
