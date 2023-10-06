aed26bd1dceac77b5867d207db7299133663fb4c
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-10-06 17:24:44 +0200

    t

--- alien_msg.js
+++ alien_msg.js
@@ -1,4 +1,4 @@
-/* We Come in Peace!   
+/* We Come in Peace!!   
 We've received what (we assume) is a message of peace and brotherhood from 
 an alien planet. They almost got it right, but the messages are 
 backward. Write functions to reverse the backward messages so we can 
